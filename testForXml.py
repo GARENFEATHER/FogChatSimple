0fa38a902757323dff8886aa19bd3b3c6e0d3d60
SASUKE <garen@localhost.localdomain>
2017-03-12 18:08:25 -0700

    test xml

---
+++ testForXml.py
@@ -0,0 +1,22 @@
+# -*- coding:utf-8 -*-
+from xml.dom.minidom import Document
+
+testbmp=open("../../test.bmp","r")
+
+coreContent=Document()
+
+words=coreContent.createElement('words')
+words.setAttribute('type','text')
+content=coreContent.createTextNode('JUST SAY: ')
+words.appendChild(content)
+coreContent.appendChild(words)
+
+pic=coreContent.createElement('img')
+picCon=coreContent.createTextNode(testbmp.read())
+testbmp.close()
+pic.appendChild(picCon)
+words.appendChild(pic)
+
+afterWords=coreContent.createTextNode(' AND SAY NOTHING')
+words.appendChild(afterWords)
+print coreContent.toprettyxml(indent='')
